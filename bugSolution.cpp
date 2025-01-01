444db198cf9e44b4afc348200a7b218465685945
BugHunter-X <askzenai@gmail.com>
2025-01-01 10:46:05 +0100

    Add bugSolution.cpp file

---
+++ bugSolution.cpp
@@ -0,0 +1,7 @@
+int main() {
+  std::vector<int> vec = {1, 2, 3, 4, 5};
+  for (int i = 0; i < vec.size(); ++i) { // Corrected: Iterate up to vec.size() -1
+    std::cout << vec[i] << " ";
+  }
+  return 0;
+} 
